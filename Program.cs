c9bebf6d812a2150fa4dbdf6b8131789a5ea05d5
Isaac Perkins <isaacperkins987@gmail.com>
2024-11-19 12:49:00 -0800

    Program entry point done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Cocona;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using PixCollect.CLI;
 
-Console.WriteLine("Hello, World!");
+namespace PixCollect;
+
+// TODO: give better description
+[HasSubCommands(typeof(Scrape), Description = "Scraping commands")]
+[HasSubCommands(typeof(Upload), Description = "Uploading commands")]
+public class Program(IConfiguration configuration, ILogger<Program> logger)
+{
+    private IConfiguration _configuration = configuration;
+    private ILogger<Program> _logger = logger;
+
+    public static void Main(string[] args)
+    {
+        CoconaApp.CreateHostBuilder()
+            .ConfigureLogging(logging =>
+            {
+                logging.AddDebug();
+            })
+            .ConfigureServices(services =>
+            {
+                // Service Injection goes here
+            }).Run<Program>(args);
+    }
+}
