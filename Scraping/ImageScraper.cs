5f8c87101cc5aa730b8f6fbce617246b99f408c7
Isaac Perkins <isaacperkins987@gmail.com>
2024-11-20 11:29:41 -0800

    Added puppeteer browser

--- Scraping/ImageScraper.cs
+++ Scraping/ImageScraper.cs
@@ -1,49 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Concurrent;
 using Microsoft.Extensions.Logging;
+using PuppeteerSharp;
 
 namespace PixCollect.Scraping;
 
 public class ImageScraper
 {
-    private readonly string _imageSource;
-    private readonly ScrapeSettings _scrapeSettings;
-    private readonly ILogger<ImageScraper> _logger;
+    private readonly IPage _page;
     private readonly BlockingCollection<Uri> _imageUrls;
     private readonly ImageParser _imageParser;
     private readonly ImageDownloader _imageDownloader;
     
-    public ImageScraper(string imageSource, ScrapeSettings scrapeSettings, ILogger<ImageScraper> logger)
+    public ImageScraper(string imageSource, IPage page, ScrapeSettings scrapeSettings)
     {
-        _imageSource = imageSource;
-        _scrapeSettings = scrapeSettings;
-        _logger = logger;
+        _page = page;
         _imageUrls = new BlockingCollection<Uri>();
-        _imageParser = GetImageParser();
-        _imageDownloader = new ImageDownloader();
+        _imageParser = GetImageParser(imageSource);
+        _imageDownloader = new ImageDownloader(scrapeSettings);
     }
     
     public async Task<int> ScrapeAsync(string query, int limit, CancellationToken cancellationToken)
     {
-        
         // Parse and download images
         Task<int> parse = _imageParser.ParseImagesAsync(query, limit, cancellationToken);
         Task<int> download = _imageDownloader.DownloadImagesAsync(cancellationToken);
         int[] total = await Task.WhenAll(parse, download);
         
-        _logger.LogInformation("Parsed images: total={total}", total[0]);
-        _logger.LogInformation("Downloaded images: total={total}", total[1]);
-        
         // Return the total images downloaded
         return total[1];
     }
 
-    private ImageParser GetImageParser()
+    private ImageParser GetImageParser(string imageSource)
     {
-        return _imageSource switch
+        return imageSource switch
         {
-            "google" => new GoogleImageParser(_imageUrls),
-            _ => throw new ArgumentException($"Unsupported image source: {_imageSource}")
+            "google" => new GoogleImageParser(_imageUrls, _page),
+            _ => throw new ArgumentException($"Unsupported image source: {imageSource}")
         };
     }
 }
