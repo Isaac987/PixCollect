63e96d4a7030934c4134cc90a82f25d312989f35
Isaac Perkins <isaacperkins987@gmail.com>
2024-11-20 11:29:24 -0800

    Added page support

--- Scraping/ImageParser.cs
+++ Scraping/ImageParser.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Concurrent;
+using PuppeteerSharp;
 
 namespace PixCollect.Scraping;
 
-public abstract class ImageParser(BlockingCollection<Uri> imageUrls)
+public abstract class ImageParser(BlockingCollection<Uri> imageUrls, IPage page)
 {
     public abstract Task<int> ParseImagesAsync(string query, int limit, CancellationToken cancellationToken);
 }
