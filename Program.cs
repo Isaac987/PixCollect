971f28323c7af3641a9ffd29199ccc44a020b1c8
Isaac Perkins <isaacperkins987@gmail.com>
2024-11-20 09:20:34 -0800

    Added configuration

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using PixCollect.CLI;
+using PixCollect.Scraping;
 
 namespace PixCollect;
 
@@ -17,12 +18,21 @@ public class Program(IConfiguration configuration, ILogger<Program> logger)
     public static void Main(string[] args)
     {
         CoconaApp.CreateHostBuilder()
+            .ConfigureAppConfiguration(config =>
+            {
+                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            })
             .ConfigureLogging(logging =>
             {
                 logging.AddDebug();
             })
-            .ConfigureServices(services =>
+            .ConfigureServices((context, services) =>
             {
+                // Bind configuration to the class
+                ScrapeSettings scrapeSettings = new();
+                context.Configuration.GetSection("ScrapingSettings").Bind(scrapeSettings);
+                services.AddSingleton(scrapeSettings);
+                
                 // Service Injection goes here
             }).Run<Program>(args);
     }
