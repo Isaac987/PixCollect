165d1a6b15839e56439d37bf11ce4ed8f540cd19
Isaac Perkins <isaacperkins987@gmail.com>
2024-11-20 09:20:57 -0800

    Reformatted and implemented scrape run

--- CLI/Scrape.cs
+++ CLI/Scrape.cs
@@ -1,49 +1,65 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using Cocona;
-using Microsoft.Extensions.Configuration;
+using Cocona.Application;
 using Microsoft.Extensions.Logging;
+using PixCollect.Scraping;
 
 namespace PixCollect.CLI;
 
-public class Scrape(IConfiguration configuration, ILogger<Scrape> logger)
-{   
-    private IConfiguration _configuration = configuration;
-    private ILogger<Scrape> _logger = logger;
-    
+public class Scrape(ScrapeSettings scrapeSettings, ILogger<Scrape> logger, ILoggerFactory loggerFactory)
+{
     [Command(Description = "Scrape images from the web using a search term.")]
-    public async Task ScrapeImages(
-        [Argument(Description = "The keyword or phrase to search for when scraping images.")] string query,
-        [Argument(Description = "The maximum number of images to download."), Range(1, int.MaxValue, ErrorMessage = "Limit must be greater than 0")] int limit,
-        [Option(Description = "The directory where the downloaded images will be saved.")] string output = ".",
-        [Option(Description = "The image format for the downloaded files (e.g., jpg, png).")] string format = "jpg",
-        [Option(Description = "The naming pattern for the saved image files.")] string filename = ".")
+    public async Task Run(
+        [Argument(Description = "The keyword or phrase to search for when scraping images.")]
+        string query,
+        [Argument(Description = "The maximum number of images to download.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Limit must be greater than 0")]
+        int limit,
+        [FromService] ICoconaAppContextAccessor contextAccessor)
     {
-        logger.LogInformation("Starting Scrape Task");
+        logger.LogInformation("Starting Image Scrape: query={query}, limit={limit}", query, limit);
+
+        CoconaAppContext context = contextAccessor.Current ?? throw new InvalidOperationException();
+        IEnumerable<Task<int>> scrapingTasks = scrapeSettings.ScrapingSources.Select(async scrapeSource =>
+        {
+            ImageScraper scraper = new(scrapeSource, scrapeSettings, loggerFactory.CreateLogger<ImageScraper>());
+            return await scraper.ScrapeAsync(query, limit, context.CancellationToken);
+        });
+        
+        int total = (await Task.WhenAll(scrapingTasks)).Sum();
+        
+        logger.LogInformation($"Ending Image Scrape: total={total}");
     }
-    
+
     [Command(Description = "Enable a specific image scraping source.")]
-    public async Task EnableSource([Argument(Description = "The name of the image source to enable, such as 'Google' or 'Unsplash'.")] string source) 
+    public async Task EnableSource(
+        [Argument(Description = "The name of the image source to enable, such as 'Google' or 'Unsplash'.")]
+        string source)
     {
         logger.LogInformation($"Enabling source: {source}");
     }
-    
+
     [Command(Description = "Disable a specific image scraping source.")]
-    public async Task DisableSource([Argument(Description = "The name of the image source to disable, such as 'Google' or 'Unsplash'.")] string source) 
+    public async Task DisableSource(
+        [Argument(Description = "The name of the image source to disable, such as 'Google' or 'Unsplash'.")]
+        string source)
     {
         logger.LogInformation($"Disabling source: {source}");
     }
-    
+
     [Command(Description = "Set a default configuration value for the scraper.")]
     public async Task SetDefaultValue(
-        [Argument(Description = "The configuration setting to modify (e.g., 'output', 'format').")] string setting, 
-        [Argument(Description = "The new value to assign to the specified configuration setting.")] string value)
+        [Argument(Description = "The configuration setting to modify (e.g., 'output', 'format').")]
+        string setting,
+        [Argument(Description = "The new value to assign to the specified configuration setting.")]
+        string value)
     {
-        _logger.LogInformation($"Setting default value: {setting} = {value}");
+        logger.LogInformation($"Setting default value: {setting} = {value}");
     }
 
     [Command(Description = "List all current configuration settings.")]
     public async Task ListSettings()
     {
-        _logger.LogInformation("Listing all current settings...");
+        logger.LogInformation("Listing all current settings...");
     }
 }
