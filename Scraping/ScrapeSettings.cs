568a0453980b2667085d3092e61483633f30e2b3
Isaac Perkins <isaacperkins987@gmail.com>
2024-11-20 09:21:10 -0800

    Basic settings class

---
+++ Scraping/ScrapeSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace PixCollect.Scraping;
+
+public class ScrapeSettings
+{
+    public string OutputDirectory { get; set; }
+    public string Format { get; set; }
+    public string Filename { get; set; }
+    public string[] ScrapingSources { get; set; }
+}
