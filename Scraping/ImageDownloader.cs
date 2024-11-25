b59ea3a6f43e95f62e6975d58cb1eae675a7a3a7
Isaac Perkins <isaacperkins987@gmail.com>
2024-11-25 12:17:02 -0800

    Implemented downloader

--- Scraping/ImageDownloader.cs
+++ Scraping/ImageDownloader.cs
@@ -1,9 +1,42 @@
-﻿namespace PixCollect.Scraping;
+﻿using System.Collections.Concurrent;
+using SixLabors.ImageSharp;
 
-public class ImageDownloader
+namespace PixCollect.Scraping;
+
+public class ImageDownloader(BlockingCollection<Uri> imageUrls, string imageSource, ScrapeSettings scrapeSettings, IHttpClientFactory httpClientFactory)
 {
     public async Task<int> DownloadImagesAsync(CancellationToken cancellationToken)
     {
-        return 0;
+        int imageCount = 0;
+        using HttpClient httpClient = httpClientFactory.CreateClient();
+
+        System.IO.Directory.CreateDirectory(scrapeSettings.OutputDirectory);
+        
+        string sourcename = scrapeSettings.Filename.Replace("{s}", imageSource);
+        
+        foreach (Uri imageUrl in imageUrls.GetConsumingEnumerable(cancellationToken))
+        {
+            try
+            {
+                Console.WriteLine($"Downloading image {imageUrl}");
+                
+                HttpResponseMessage response = await httpClient.GetAsync(imageUrl, cancellationToken);
+                response.EnsureSuccessStatusCode();
+                byte[] imageBytes = await response.Content.ReadAsByteArrayAsync(cancellationToken);
+                using Image image = Image.Load(imageBytes);
+                
+                string filename = sourcename.Replace("{i}", imageCount.ToString());
+                string outputPath = Path.Combine(scrapeSettings.OutputDirectory, filename);
+                
+                await image.SaveAsync(outputPath + '.' + scrapeSettings.Format, cancellationToken);
+                imageCount++;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Error downloading {imageUrl}: {e.Message}");
+            }
+        }
+        
+        return imageCount;
     }
 }
