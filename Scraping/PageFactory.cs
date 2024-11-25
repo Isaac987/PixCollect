eb7aa60351216c3d22215e09213ed2d8d022e552
Isaac Perkins <isaacperkins987@gmail.com>
2024-11-25 12:17:37 -0800

    Browser page manager

---
+++ Scraping/PageFactory.cs
@@ -0,0 +1,40 @@
+﻿using PuppeteerSharp;
+
+namespace PixCollect.Scraping;
+
+public sealed class PageFactory : IDisposable, IAsyncDisposable
+{
+    private readonly IBrowser _browser;
+    
+    private PageFactory(IBrowser browser)
+    {
+        _browser = browser;
+    }
+
+    public void Dispose()
+    {
+        _browser.Dispose();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _browser.DisposeAsync();
+    }
+
+    public static async Task<PageFactory> CreateAsync()
+    {
+        // TODO: Check if browser is installed
+        // Download the browser executable if absent
+        Console.WriteLine("Downloading browser executable...");
+        await new BrowserFetcher().DownloadAsync();
+        
+        IBrowser browser = await Puppeteer.LaunchAsync(new LaunchOptions() { Headless = false });
+        
+        return new PageFactory(browser);
+    }
+
+    public async Task<IPage> CreatePageAsync()
+    {
+        return await _browser.NewPageAsync();
+    }
+}
