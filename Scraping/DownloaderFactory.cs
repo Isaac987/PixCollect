0747ff309c49295abfba522c19225362189cda58
Isaac Perkins <isaacperkins987@gmail.com>
2024-11-29 00:05:02 -0800

    Factory provides more consistent creation with DI

---
+++ Scraping/DownloaderFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace PixCollect.Scraping;
+
+public sealed class DownloaderFactory(
+    ScrapeConfiguration scrapeConfiguration,
+    IHttpClientFactory httpClientFactory,
+    ILoggerFactory loggerFactory) : IDisposable
+{
+    public Downloader GetDownloader()
+    {
+        return new Downloader(scrapeConfiguration, httpClientFactory.CreateClient(),
+            loggerFactory.CreateLogger<Downloader>());
+    }
+
+    public void Dispose()
+    {
+    }
+}
