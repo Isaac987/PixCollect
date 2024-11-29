379283e07eeff13c2616079b6cd895eab9a374e4
Isaac Perkins <isaacperkins987@gmail.com>
2024-11-28 23:29:30 -0800

    Obsolete

--- Scraping/ImageParser.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Concurrent;
-using PuppeteerSharp;
-
-namespace PixCollect.Scraping;
-
-public abstract class ImageParser(BlockingCollection<Uri> imageUrls, IPage page)
-{
-    public abstract Task<int> ParseImagesAsync(string query, int limit, CancellationToken cancellationToken);
-}
