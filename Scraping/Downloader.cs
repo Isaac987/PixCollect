d49e4d4065d5423b3c73167d1d5e557598b5728f
Isaac Perkins <isaacperkins987@gmail.com>
2024-11-29 00:04:32 -0800

    Refactored name

---
+++ Scraping/Downloader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Concurrent;
+using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp;
+
+namespace PixCollect.Scraping;
+
+public sealed class Downloader(
+    ScrapeConfiguration scrapeConfiguration,
+    HttpClient httpClient,
+    ILogger<Downloader> logger) : IDisposable
+{
+    public async Task<int> DownloadAsync(HashSet<string> imageUrls, CancellationToken cancellationToken)
+    {
+        int downloaded = 0;
+        
+        foreach (string url in imageUrls)
+        {
+                
+        }
+        
+        return downloaded;
+    }
+
+    public void Dispose()
+    {
+        httpClient.Dispose();
+    }
+}
