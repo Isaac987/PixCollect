4eb1c8cb5413a5d6457ce3b2c30cd2d9190ba9db
Isaac Perkins <isaacperkins987@gmail.com>
2024-11-29 00:06:23 -0800

    Base class for site parsers

---
+++ Scraping/SiteParser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using PuppeteerSharp;
+
+namespace PixCollect.Scraping;
+
+public abstract class SiteParser(IPage page, ILogger<SiteParser> logger) : IAsyncDisposable
+{
+    public abstract Task<int> ParseAsync(string query, int limit, HashSet<string> imageUrls, CancellationToken cancellationToken);
+
+    public async ValueTask DisposeAsync()
+    {
+        await page.CloseAsync();
+    }
+}
