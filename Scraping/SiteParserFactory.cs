6e28aeef65349a9f6ad209b7690b20ace63bbd80
Isaac Perkins <isaacperkins987@gmail.com>
2024-11-28 23:30:25 -0800

    Manages browser, pages, and parsers

---
+++ Scraping/SiteParserFactory.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using PuppeteerSharp;
+
+namespace PixCollect.Scraping;
+
+public sealed class SiteParserFactory(ILoggerFactory loggerFactory, ILogger<SiteParserFactory> logger) : IDisposable
+{
+    private IBrowser? _browser;
+    
+    public async Task<SiteParser> GetParserAsync(string scrapingSource)
+    {
+        // Get the browser
+        _browser ??= await GetBrowser();
+        
+        // Get the page
+        IPage page = await _browser.NewPageAsync();
+        
+        return scrapingSource switch
+        {
+            "google" => new GoogleParser(page, loggerFactory.CreateLogger<GoogleParser>()),
+            _ => throw new ArgumentException($"Unsupported image source: {scrapingSource}")
+        };
+    }
+    
+    public void Dispose()
+    {
+        _browser?.Dispose();
+    }
+    
+    private async Task<IBrowser> GetBrowser()
+    {
+        // TODO: check if browser is installed
+        logger.LogTrace("Downloading browser");
+        await new BrowserFetcher().DownloadAsync();
+        
+        logger.LogTrace("Launching browser");
+        _browser = await Puppeteer.LaunchAsync(new LaunchOptions() { Headless = false });
+
+        return _browser;
+    }
+}
