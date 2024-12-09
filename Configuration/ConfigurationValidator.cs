d30f2b5a44fbcf411e8e735d17da71325b603a97
Isaac Perkins <isaacperkins987@gmail.com>
2024-12-09 10:56:53 -0800

    basic configuration validator

---
+++ Configuration/ConfigurationValidator.cs
@@ -0,0 +1,19 @@
+﻿namespace PixCollect.Configuration;
+
+public static class ConfigurationValidator
+{
+    // TODO: read from file
+    private static readonly HashSet<string> Sources = ["google", "pixabay"];
+    private static readonly HashSet<string> Formats = ["bmp", "gif", "jpeg", "jpg", "pbm", "png", "tiff", "tif", "tga", "webp"];
+    
+    public static bool IsValidSource(string source)
+    {
+        return Sources.Contains(source);
+    }
+
+    public static bool IsValidFormat(string format)
+    {
+        return Formats.Contains(format);
+    }
+    
+}
