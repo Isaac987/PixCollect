27698d595eb1a0c6d3e635abb3ea863bae65a075
Isaac Perkins <isaacperkins987@gmail.com>
2024-12-11 12:26:01 -0800

    Removed default settings

--- Configuration/ScrapeConfiguration.cs
+++ Configuration/ScrapeConfiguration.cs
@@ -2,8 +2,8 @@
 
 public sealed class ScrapeConfiguration
 {
-    public string OutputDirectory { get; set; } = "Images";
-    public string Format { get; set; } = "jpg";
-    public bool Headless { get; set; } = true;
-    public List<string> ScrapingSources { get; set; } = ["google"];
+    public string OutputDirectory { get; set; } 
+    public string Format { get; set; }
+    public bool Headless { get; set; }
+    public List<string> ScrapingSources { get; set; }
 }
